147893117127a2a804e097fd321be065c9b7cf70
David Lay <davidjlay2277@gmail.com>
2023-10-16 21:06:34 -0500

    final automation added

--- __tests__/duelDuo.test.js
+++ __tests__/duelDuo.test.js
@@ -11,25 +11,48 @@ afterEach(async () => {
 });
 
 describe("Duel Duo tests", () => {
-  test("page loads with title", async () => {
+  test("ONE: page loads with title", async () => {
     await driver.get("http://localhost:8000");
     await driver.wait(until.titleIs("Duel Duo"), 1000);
   });
-  test("Draw button displays choices", async () => {
+
+  test("TWO: Draw button displays choices", async () => {
     await driver.get("http://localhost:8000");
-//Check choices for content, should be empty
-    const choicesBeforeDraw = await driver.findElement(By.id("choices"));
-    const initialChoicesContent = await choicesBeforeDraw.getText();
-    expect(initialChoicesContent.trim()).toBe("");
-// Click draw
+// Check that the choices div is initially empty
+    const choicesElement = await driver.findElement(By.id("choices"));
+    const initialChoicesText = await choicesElement.getText();
+    expect(initialChoicesText.trim()).toBe(""); 
+// Click "Draw"
     const drawButton = await driver.findElement(By.id("draw"));
     await drawButton.click();
-//Check choices for content, should not be empty
-    const choicesAfterDraw = await driver.wait(
+    const updatedChoicesElement = await driver.wait(
       until.elementLocated(By.id("choices")),
-      2000
+      5000
     );
-    const afterDrawChoices = await choicesAfterDraw.getText();
-    expect(afterDrawChoices.trim()).not.toBe(""); // Ensure it's not empty after clicking
+// Check that choices" div is no longer empty
+    const updatedChoicesText = await updatedChoicesElement.getText();
+    expect(updatedChoicesText.trim()).not.toBe(""); // Ensure it's not empty after clicking
+  });
+  test("THREE: USer can only select 2 bots", async () => {
+    await driver.get("http://localhost:8000");
+// Click "Draw" and find choices
+    const drawButton = await driver.findElement(By.id("draw"));
+    await drawButton.click();
+    const choicesElement = await driver.findElement(By.id("choices"));
+// CLick the first three bots
+    for (let i = 1; i <= 3; i++) {
+      const botButton = await choicesElement.findElement(By.className("bot-btn"));
+      await botButton.click();
+      try {
+        const alert = await driver.switchTo().alert();
+        await alert.accept(); 
+        break; 
+      } catch (error) {
+      }
+    }
+    await driver.switchTo().defaultContent();
+// Count the bots actually added
+    const botCards = await driver.findElements(By.css("#player-duo .bot-card.outline"));
+    expect(botCards.length).toBe(2);
   });
 });
