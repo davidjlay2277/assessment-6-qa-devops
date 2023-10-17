ee6b903772c47ed35722448054722e08ce4950f2
David Lay <davidjlay2277@gmail.com>
2023-10-16 19:36:13 -0500

    autoamtion 1 working

--- __tests__/duelDuo.test.js
+++ __tests__/duelDuo.test.js
@@ -14,14 +14,22 @@ describe("Duel Duo tests", () => {
   test("page loads with title", async () => {
     await driver.get("http://localhost:8000");
     await driver.wait(until.titleIs("Duel Duo"), 1000);
-    
   });
   test("Draw button displays choices", async () => {
-    await driver.findElement(By.id("draw")).click();
-await driver.wait(until.elementIsVisible(driver.findElement(By.id("choices"))), 1000);
+    await driver.get("http://localhost:8000");
+//Check choices for content, should be empty
+    const choicesBeforeDraw = await driver.findElement(By.id("choices"));
+    const initialChoicesContent = await choicesBeforeDraw.getText();
+    expect(initialChoicesContent.trim()).toBe("");
+// Click draw
+    const drawButton = await driver.findElement(By.id("draw"));
+    await drawButton.click();
+//Check choices for content, should not be empty
+    const choicesAfterDraw = await driver.wait(
+      until.elementLocated(By.id("choices")),
+      2000
+    );
+    const afterDrawChoices = await choicesAfterDraw.getText();
+    expect(afterDrawChoices.trim()).not.toBe(""); // Ensure it's not empty after clicking
   });
 });
-
-
-
-
